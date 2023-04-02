4484b480f54fad9e7186b9fe14b9efb9955b7f74
hammadxp <hammadjaved9860@gmail.com>
2023-04-03 00:33:54 +0500

    basic interface ready, not beautiful yet, took an hour instead of half hour

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Rubik: "Rubik, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
